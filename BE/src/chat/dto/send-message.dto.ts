918c136cf7cd0ab0c32a7add8fa7f0df579174e9
Fallin7 <davide.ortu@visioscientiae.com>
2026-06-22 12:36:23 +0200

    removed BE

--- BE/src/chat/dto/send-message.dto.ts
+++
@@ -1,9 +0,0 @@
-export class SendMessageDto {
-  message: string;
-  sessionId?: string;
-
-  constructor(message: string, sessionId?: string) {
-    this.message = message;
-    this.sessionId = sessionId;
-  }
-}
--- BE/src/chat/prompts/index.ts
+++
@@ -1,4 +0,0 @@
-import { qaPrompt } from './context_injection.prompt';
-import { contextualizeQPrompt } from './history-context.prompt';
-
-export { qaPrompt, contextualizeQPrompt };
